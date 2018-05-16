162ca673d035b855873d5f901ef71b9e24277f7a
dishubagga <dishubagga9111@gmail.com>
2018-05-16 17:17:26 +0530

    node js without express practice

--- v1/without express/app1.js
+++ v1/without express/app1.js
@@ -1,2 +1,4 @@
 var addition = require("./addition.js");
+var subtract = require("./sub.js")
 console.log(addition.addNumber(1,2));
+console.log(subtract.sub(4,5));
--- v1/without express/mypgm.js
+++ v1/without express/mypgm.js
@@ -1,4 +1,7 @@
-var http = require('http'); 
-http.createServer(function (req, res) {
- res.writeHead(200, {'Content-Type': 'text/html'}); res.end('Hello World!'); 
-}).listen(4200);
+var http = require("http");
+http.createServer(function(req,res){
+    res.writeHead("200",{'content-type':'text/html'});
+    res.end("hello")
+
+}).listen(4300);
+
