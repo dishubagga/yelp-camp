db5c5784be5b3a9d2a0baf474354b5c1e3ede431
dishubagga <dishubagga9111@gmail.com>
2018-06-25 18:54:29 +0530

    shoppinglist added forms

--- v1/without express/example.js
+++ v1/without express/example.js
@@ -15,7 +15,7 @@ var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
 var q = url.parse(adr,true);
 console.log(q.host);
 console.log(q.pathname);
-console.log(q.search);
+console.log(q.query);
 
 var qdata = q.query;
 console.log(qdata.month);
